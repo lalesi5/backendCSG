62fddb489d372517b48d00570f45c8d5f78845db
Alex Carrera <a_fcalvarez@hotmail.com>
2022-11-19 21:55:08 -0500

    init

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require("dotenv");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
 app.use(cors());
 
-const port = 3001
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=>{
-    console.log(`http://localhost:${port}`)
+    console.log(`http://localhost:${port}`);
 })
